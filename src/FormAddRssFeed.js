d008e9881bd230f7c9539aa253c54b7d09b187d6
Azamat Abdullin <abdullin.azamat@clover.global>
2020-12-06 23:25:38 +0300

    copied styles from the example

--- src/FormAddRssFeed.js
+++ src/FormAddRssFeed.js
@@ -1,12 +1,26 @@
 function renderAddRSSFeedForm() {
+  const inputId = "rss-url-input";
   return `
-    <div class="jumbotron">
-      <form class="form-inline">
-        <label class="sr-only" for="inlineFormInputName2">RSS-поток</label>
-        <input type="text" class="form-control mb-2 mr-sm-2" id="inlineFormInputName2" placeholder="RSS-поток">
-
-        <button type="submit" class="btn btn-primary mb-2">Добавить</button>
-      </form>
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container-xl">
+        <div class="row">
+          <div class="col-md-10 col-lg-8 mx-auto">
+            <h1 class="display-3">RSS Reader</h1>
+            <p class="lead">Start reading RSS today! It is easy, it is nicely.</p>
+            <label for="${inputId}">RSS link. Example: https://ru.hexlet.io/lessons.rss</label>
+            <form class="rss-form">
+              <div class="form-row">
+                <div class="col">
+                  <input type="text" class="form-control form-control-lg w-100" id="${inputId}">
+                </div>
+                <div class="col-auto">
+                  <button type="submit" class="btn btn-lg btn-primary px-sm-5">Add</button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   `;
 }
