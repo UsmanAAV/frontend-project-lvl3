876e2445d68830d535f26185e6c77694a84d483a
Azamat Abdullin <abdullin.azamat@clover.global>
2021-03-06 21:13:47 +0300

    fixed codeclimate issues

--- src/render/renderFeeds.js
+++ src/render/renderFeeds.js
@@ -4,9 +4,6 @@ import i18next from 'i18next';
 function renderFeeds(value) {
   const feeds = document.querySelector('.feeds');
 
-  if (_.size(value) === 0) {
-    return;
-  }
   feeds.innerHTML = `
     <h2>${i18next.t('feeds')}</h2>
     <ul class="list-group mb-5">${_.map(
