4b2da266a177925d7fbadd22f9717b3811f1cf3a
Azamat Abdullin <abdullin.azamat@clover.global>
2021-03-09 17:29:58 +0300

    added init.js

--- src/app.js
+++ src/app.js
@@ -1,37 +1,8 @@
 /* eslint-disable no-param-reassign */
-import * as onChange from 'on-change';
-import i18next from 'i18next';
-import 'bootstrap';
-import { FORM_STATE } from './constants';
-import { render } from './render';
-import { getSubmitHandler } from './submit';
-import { ru } from './dictionaries';
-import { getClickHandler } from './click';
-import './scss/app.scss';
+import { init } from './init';
 
-const app = async () => {
-  await i18next.init({
-    lng: 'ru',
-    resources: { ru },
-  });
-
-  const state = {
-    form: {
-      state: FORM_STATE.initial,
-      feedback: '',
-    },
-    feeds: [],
-    posts: [],
-    openedPostId: null,
-    readPosts: [],
-  };
-
-  const watchedState = onChange(state, render);
-
-  const form = document.getElementById('rss-form');
-  form.addEventListener('submit', getSubmitHandler(watchedState));
-  const posts = document.querySelector('.posts');
-  posts.addEventListener('click', getClickHandler(watchedState));
+const app = () => {
+  init();
 };
 
 export { app };
