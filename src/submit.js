80bfd829539e2896203476e21071c0e91c4a9c40
Azamat Abdullin <abdullin.azamat@clover.global>
2021-03-09 20:26:47 +0300

    is my js run?

--- src/submit.js
+++ src/submit.js
@@ -64,6 +64,7 @@ function getSubmitHandler(state) {
 
   return function handler(e) {
     e.preventDefault();
+    console.log('IS MY JAVASCRIPT RUN?');
     const formData = new FormData(e.target);
     const url = formData.get('rss-url-input');
 
